47c220aa97aec0596d5d9912df664d4d3097153f
luan duong <luan.dph@gmail.com>
2022-02-28 09:17:34 +0700

    components

---
+++ src/components/TestComponents.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Tabs} from './tabs/tabs';
+
+export const TestComponent = () => {
+  return (
+    <div className="">
+      <Tabs/>
+    </div>
+  )
+};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ExpenseTrackerApp from './ExpenseTrackerUseReducer/ExpenseTrackerApp';
-import reportWebVitals from './reportWebVitals';
-import {ExpenseTrackerUseState} from './ExpenseTrackerUseState/ExpenseTrackerUseState';
-import ExpenseTrackerClassComponent from './ExpenseTrackerClassComponent/ExpenseTrackerClassComponent'
-import {ExpenseTrackerUseStateNoContext} from './ExpenseTrackerUseStateNoContext/ExpenseTrackerUseStateNoContext'
-import ExpenseTrackerClassNoContext from './ExpenseTrackerClassNoContext/ExpenseTrackerClassNoContext'
+// import ExpenseTrackerApp from './ExpenseTrackerUseReducer/ExpenseTrackerApp';
+// import {ExpenseTrackerUseState} from './ExpenseTrackerUseState/ExpenseTrackerUseState';
+// import ExpenseTrackerClassComponent from './ExpenseTrackerClassComponent/ExpenseTrackerClassComponent'
+// import {ExpenseTrackerUseStateNoContext} from './ExpenseTrackerUseStateNoContext/ExpenseTrackerUseStateNoContext'
+// import ExpenseTrackerClassNoContext from './ExpenseTrackerClassNoContext/ExpenseTrackerClassNoContext'
+import {TestComponent} from './components/TestComponents';
+
 ReactDOM.render(
   <React.StrictMode>
     {/* <ExpenseTrackerClassComponent/> */}
     {/* <ExpenseTrackerApp /> */}
     {/* <ExpenseTrackerUseState /> */}
     {/* <ExpenseTrackerUseStateNoContext/> */}
-    <ExpenseTrackerClassNoContext/>
+    {/* <ExpenseTrackerClassNoContext/> */}
+    <TestComponent/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-reportWebVitals();
